8733bfc419a7bf6080db3b8f79bb835cdaf6724f
sandoval77 <145803017+sandoval77@users.noreply.github.com>
2023-11-14 19:51:40 -0800

    Create Lab_4_vec.cpp

---
+++ Lab_4_vec.cpp
@@ -0,0 +1,26 @@
+/***
+* CSE 2010 Fall 2023
+* Lab #{n}
+* {Student Name} #{Coyote ID}
+* {Date}
+*
+* {Description of lab AKA paraphrase "abstract" section from lab}
+*
+* {Describe 1) what was most challenging and 2) how you solved/overcame the
+problem OR
+* the directions of how you PLAN to solve it}
+*
+***/
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+int main()
+{
+    // initialize vec
+    //initialize ArrayVec and DisplayVec
+  
+    // populate with values
+    // display contents
+}
