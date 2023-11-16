512ade4e88c3e5bda74f1172cf7646539ce0be8a
sandoval77 <145803017+sandoval77@users.noreply.github.com>
2023-11-15 17:31:06 -0800

    Update DisplayVec.cpp

--- DisplayVec.cpp
+++ DisplayVec.cpp
@@ -1,1 +1,6 @@
 //DisplayVec.cpp
+DisplayVec();
+      void printVector(const vector<int> v);
+      int getVectMemSize(const vector<int> v);
+      void printMetaVector(const vector<int> v); 
+      const int * getAddress(const vector<int> v, int index); 
