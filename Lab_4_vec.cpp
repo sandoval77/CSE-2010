cc9504184085c74e189a0d9a835a0686e09b7833
sandoval77 <145803017+sandoval77@users.noreply.github.com>
2023-11-15 17:31:12 -0800

    Update Lab_4_vec.cpp

--- Lab_4_vec.cpp
+++ Lab_4_vec.cpp
@@ -1,13 +1,12 @@
 /***
 * CSE 2010 Fall 2023
-* Lab #{n}
-* {Student Name} #{Coyote ID}
-* {Date}
+* Lab #4
+* Daisy Sandoval 
+* 11/15/23
 *
-* {Description of lab AKA paraphrase "abstract" section from lab}
+* There are eight different files that I have to link and orginize using data structures, arrays,and vectors. 
 *
-* {Describe 1) what was most challenging and 2) how you solved/overcame the
-problem OR
+* {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
 * the directions of how you PLAN to solve it}
 *
 ***/
@@ -18,9 +17,19 @@ using namespace std;
 
 int main()
 {
-    // initialize vec
-    //initialize ArrayVec and DisplayVec
+    vector<int> test_vec; //initialize vec
+    
+    DisplayVec disp = DisplayVec();
+    ArrayVec av = ArrayVec(); //initialize ArrayVec and DisplayVec
   
-    // populate with values
+    int range = 15;
+    int num_elem = 10; 
+    av.randGenVec(test_vec, num_elem, range);// populate with values
+    
+    int test_vec_size = av.getSize(test_vec.data(), test_vec.size()); 
+    cout << "Vector Element\n";
+    disp.printVector(test_vec); 
+    cout << "Array Memsize\n";
+    disp.printMetaVector(test_vec, test_vec_size) //is the Test_vec_size correct ?
     // display contents
 }
